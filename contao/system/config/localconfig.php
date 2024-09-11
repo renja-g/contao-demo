5968764c3b8f1d4dcf6b2c919c2f58e230902b5e
Renja <renja.grotemeyer@gmail.com>
2024-09-11 16:14:56 +0200

    base demo

---
+++ contao/system/config/localconfig.php
@@ -0,0 +1,6 @@
+<?php
+
+### INSTALL SCRIPT START ###
+$GLOBALS['TL_CONFIG']['licenseAccepted'] = true;
+$GLOBALS['TL_CONFIG']['installPassword'] = '$2y$10$RkxxFmYwG03DA6BX5u9WJu1MHEfQGwFSvF8.NyXmpfy.NnkWEM6MO';
+### INSTALL SCRIPT STOP ###
